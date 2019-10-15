e9a58a98e3d5c8d464aefc183ba316904321d307
Fuli <fuli@dbx.hu>
2019-10-15 07:36:25 +0200

    Constants added

--- src/main/java/hu/imsi/mir/util/Constants.java
+++ src/main/java/hu/imsi/mir/util/Constants.java
@@ -3,4 +3,17 @@ package hu.imsi.mir.util;
 public class Constants {
 
     public static final String APPLICATION_VND_API_JSON = "application/vnd.api+json";
+
+    //Arguments
+    public static String HOST = "host";
+    public static String PORT = "port";
+    public static String CONFIG = "config";
+    public static String INTERVAL = "interval";
+    public static String DB_INIT_PROPERTIES = "dbInitProperties";
+    public static String DB_CLEAR = "dbClear";
+    public static String DEFAULT_PERMISSION = "defaultPermission";
+
+    //dbClear argument values
+    public static String TRUE = "true";
+    public static String FALSE = "true";
 }
