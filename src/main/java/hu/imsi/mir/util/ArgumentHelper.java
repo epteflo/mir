d68b770e9cb2c01ee34e0d82cbc887e335a5951d
Fuli <fuli@dbx.hu>
2019-10-15 07:10:22 +0200

    Argument process class created

---
+++ src/main/java/hu/imsi/mir/util/ArgumentHelper.java
@@ -0,0 +1,51 @@
+package hu.imsi.mir.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public class ArgumentHelper {
+
+    private static final List<String> ARGUMENTS = new ArrayList<String>();
+    static {
+        ARGUMENTS.add(Constants.HOST);
+        ARGUMENTS.add(Constants.PORT);
+        ARGUMENTS.add(Constants.CONFIG);
+        ARGUMENTS.add(Constants.INTERVAL);
+        ARGUMENTS.add(Constants.DB_INIT_PROPERTIES);
+        ARGUMENTS.add(Constants.DEFAULT_PERMISSION);
+        ARGUMENTS.add(Constants.DB_CLEAR);
+    }
+
+    public static HashMap<String,String> createArgsMap(String[] args){
+        HashMap<String,String> map = new HashMap<>();
+        String key=null;
+        String value=null;
+        for(int i = 0;i<args.length;i++){
+            if( i % 2 == 0){
+                key=null;value=null;
+                //vezérlő paraméter
+                if(!args[i].startsWith("-")) return null;
+                key=args[i].substring(1);
+            } else {
+                // érték
+                value=args[i];
+                map.put(key,value);
+            }
+        }
+        return map;
+    }
+
+    public static void printHelp(){
+            System.out.println("Wrong argument(s)!");
+            System.out.println("arguments: -port <port> -interval <interval> -config <config.properties> [-host <host> -dbInitProperties <dbinit.properties> -dbClear <true|onlyLog|false>] -defaultPermission [deny|allow]");
+    }
+
+    public static boolean checkArgumentKeys(Set<String> keys){
+        for(String key : keys){
+            if(!ARGUMENTS.contains(key)) return false;
+        }
+        return true;
+    }
+}
