21b4844887567c6ff5deed5144bd6b8e204e93d7
Fuli <fuli@dbx.hu>
2019-10-15 07:07:36 +0200

    Operation and Management API class created

---
+++ src/main/java/hu/imsi/mir/services/JsonMIRManagementAPIv1.java
@@ -0,0 +1,24 @@
+package hu.imsi.mir.services;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import static hu.imsi.mir.util.Constants.APPLICATION_VND_API_JSON;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.OK;
+
+@Path("/management/v1")
+@Consumes({APPLICATION_VND_API_JSON, APPLICATION_JSON})
+@Produces({APPLICATION_VND_API_JSON, APPLICATION_JSON})
+public class JsonMIRManagementAPIv1 {
+
+    @GET
+    @Path("/info")
+    @Produces({"text/html"})
+    public Response info() {
+        return Response.status(OK).entity(getClass().getSimpleName()).build();
+    }
+}
