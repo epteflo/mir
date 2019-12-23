5f18155927ee6f7a635771951e935436553ed094
Fuli <fuli@dbx.hu>
2019-12-23 22:23:50 +0100

    Loggerservice and user

--- src/main/java/hu/imsi/mir/utils/LoggerServiceHandler.java
+++ src/main/java/hu/imsi/mir/utils/LoggerServiceHandler.java
@@ -1,8 +1,10 @@
 package hu.imsi.mir.utils;
 
 import hu.imsi.mir.dao.ServiceLogRepository;
+import hu.imsi.mir.dao.UserRepository;
 import hu.imsi.mir.dao.entities.HServiceLog;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.init.UncategorizedScriptException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,11 +13,15 @@ public class LoggerServiceHandler {
     @Autowired
     private ServiceLogRepository serviceLogRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public void logStart(String userName, String message, String sourceModule, String sourceMethod){
+        userRepository.findByName()
         HServiceLog serviceLog = new HServiceLog();
         serviceLog.setMessage(message);
         serviceLog.setSourceModul(sourceModule);
-
+        serviceLog.setSourceMethod(sourceMethod);
 
     }
 }
