0c48cc3b3d31f0089aa86403104c8d34f75660fe
Fuli <fuli@dbx.hu>
2020-08-09 22:38:54 +0200

    Poiresource modified according to the Poi extension

--- src/main/java/hu/imsi/mir/PoiResource.java
+++ src/main/java/hu/imsi/mir/PoiResource.java
@@ -38,7 +38,11 @@ public class PoiResource extends BaseResource{
                                                      @RequestParam(value = "shortDesc", required = false) final String shortDesc,
                                                      @RequestParam(value = "description", required = false) final String description,
                                                      @RequestParam(value = "category", required = false) final String category,
-                                                     @RequestParam(value = "style", required = false) final String style)
+                                                     @RequestParam(value = "style", required = false) final String style,
+                                                     @RequestParam(value = "author", required = false) final String author,
+                                                     @RequestParam(value = "age", required = false) final String age,
+                                                     @RequestParam(value = "creationPlace", required = false) final String creationPlace,
+                                                     @RequestParam(value = "material", required = false) final String material)
     {
 
         final HPoi example = new HPoi();
@@ -48,13 +52,22 @@ public class PoiResource extends BaseResource{
         example.setDescription(description);
         example.setCategory(category);
         example.setStyle(style);
+        example.setAuthor(author);
+        example.setAge(age);
+        example.setCreationPlace(creationPlace);
+        example.setMaterial(material);
         final ExampleMatcher matcher = ExampleMatcher.matchingAll()
                 .withMatcher("name", ExampleMatcher.GenericPropertyMatchers.startsWith().ignoreCase())
                 .withMatcher("type", ExampleMatcher.GenericPropertyMatchers.startsWith().ignoreCase())
                 .withMatcher("shortDesc", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase())
                 .withMatcher("description", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase())
                 .withMatcher("category", ExampleMatcher.GenericPropertyMatchers.startsWith().ignoreCase())
-                .withMatcher("style", ExampleMatcher.GenericPropertyMatchers.startsWith().ignoreCase());
+                .withMatcher("style", ExampleMatcher.GenericPropertyMatchers.startsWith().ignoreCase())
+                .withMatcher("author", ExampleMatcher.GenericPropertyMatchers.startsWith().ignoreCase())
+                .withMatcher("age", ExampleMatcher.GenericPropertyMatchers.startsWith().ignoreCase())
+                .withMatcher("creationPlace", ExampleMatcher.GenericPropertyMatchers.startsWith().ignoreCase())
+                .withMatcher("material", ExampleMatcher.GenericPropertyMatchers.startsWith().ignoreCase())
+                ;
 
         return super.getModels(matcher, example, Poi.class, RsPoi.class, userName, "getPois");
     }
