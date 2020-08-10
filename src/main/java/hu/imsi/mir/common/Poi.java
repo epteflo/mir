a56b8589f46789e1d875cd42eb0c8e4695d71c4e
Fuli <fuli@dbx.hu>
2020-08-10 09:56:44 +0200

    DTO and common objects of POI has been updated

--- src/main/java/hu/imsi/mir/common/Poi.java
+++ src/main/java/hu/imsi/mir/common/Poi.java
@@ -3,12 +3,20 @@ package hu.imsi.mir.common;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import java.util.Date;
+
 @Getter
 @Setter
 public class Poi extends Response{
     private Integer id;
     private String name;
     private String type;
+    private String author;
+    private Date creationDate;
+    private String age;
+    private String creationPlace;
+    private String material;
     private String shortDesc;
     private String description;
     private String category;
--- src/main/java/hu/imsi/mir/dto/RsPoi.java
+++ src/main/java/hu/imsi/mir/dto/RsPoi.java
@@ -3,12 +3,19 @@ package hu.imsi.mir.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class RsPoi extends RsResponse{
     private Integer id;
     private String name;
     private String type;
+    private String author;
+    private Date creationDate;
+    private String age;
+    private String creationPlace;
+    private String material;
     private String shortDesc;
     private String description;
     private String category;
